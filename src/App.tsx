855c8a51f551633c3caa8baad0be2e9d4442e652
Eugenio <eugenilopesarantesjunior@gmail.com>
2022-04-25 14:44:14 -0300

    creating header, dashboard and modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-import styled from 'styled-components'
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import Modal from 'react-modal';
+import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8285e6;
-`
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
-    <div className="App">
-      <Title>Hello World</Title>
-    </div>
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
+      <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle />
+    </>
   );
 }
