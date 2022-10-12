5125a7f501953baa29839806ece1abb6c283f174
Eugenio <eugeniolopesarantesjunior@gmail.com>
2022-10-12 15:27:28 -0300

    removing unused table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,12 +1,10 @@
 import { Summary } from "../Summary";
-import { TimesTable } from "../TimesTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
       <Summary/>
-      <TimesTable/>
     </Container>
   );
 }
